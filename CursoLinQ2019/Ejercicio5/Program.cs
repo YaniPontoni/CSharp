4867b25e41bab0435f19de3f01d761ca1a28e643
Yani Pontoni <56703256+YaniPontoni@users.noreply.github.com>
2022-05-23 18:37:11 -0300

    Add files via upload

---
+++ CursoLinQ2019/Ejercicio5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // por defecto ordena ascendentemente, para ordenar descendentemente
+            // tengo que agregar descending a la derecha de la variable
+
+            string[] nombres = { "Jorge", "Aldair", "Pedro", "Luis" };
+
+            var consulta = from nombre in nombres
+                           where nombre.StartsWith("P")
+                           orderby nombre
+                           select nombre;
+
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CursoLinQ2019/Ejercicio6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 9, 11, 4, 7, 12, 16, 15 };
+
+            var consulta = from numero in numeros
+                           orderby numero descending
+                           select numero;
+
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
